531593b351795253731a294e1156d3989d0deb9e
loborojoparaguay-cyber <loborojoparaguay@gmail.com>
2026-08-09 21:34:22 -0300

    Update servicio_musical_exclusivo.dart

--- lib/ui/screens/servicio_musical_exclusivo.dart
+++ lib/ui/screens/servicio_musical_exclusivo.dart
@@ -21,7 +21,7 @@ class ExclusiveMusicService {
     // Reemplaza con la URL o IP de tu VPS si es necesario
     try {
       final response = await http.get(Uri.parse('http://128.254.190.44:3000/api/musica'));
-      if (response.statusCode == 200) {
+  if (response.statusCode == 200) {
         List<dynamic> body = jsonDecode(response.body);
         return body.map((dynamic item) => DriveSong.fromJson(item)).toList();
       }
