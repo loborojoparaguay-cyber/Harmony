42815902a8ce866f4d76abca62343c41412807e7
loborojoparaguay-cyber <loborojoparaguay@gmail.com>
2026-08-15 16:42:51 -0300

    Update player_controller.dart

--- lib/ui/player/player_controller.dart
+++ lib/ui/player/player_controller.dart
@@ -99,7 +99,6 @@ class PlayerController extends GetxController
   }
 
   void _init() async {
-    //_createAppDocDir();
     _listenForChangesInPlayerState();
     _listenForChangesInPosition();
     _listenForChangesInBufferedPosition();
@@ -121,7 +120,6 @@ class PlayerController extends GetxController
       initGesturePlayerStateAnimationController();
     }
 
-    // only for android auto
     if (GetPlatform.isAndroid) {
       _listenForCustomEvents();
     }
@@ -184,14 +182,13 @@ class PlayerController extends GetxController
       }
 
       final settings = Get.find<SettingsScreenController>();
-      // Keep the screen awake whenever playback is active and the setting is enabled.
       final shouldEnable = settings.keepScreenAwake.isTrue && isPlaying;
       _setWakelock(shouldEnable);
     });
   }
 
   void _setWakelock(bool enable) {
-    if (_wakelockActive == enable) return; // no-op if already in desired state
+    if (_wakelockActive == enable) return;
 
     try {
       if (enable) {
@@ -273,7 +270,6 @@ class PlayerController extends GetxController
           Navigator.pop(Get.context!);
         }
 
-        // reset player visible state when player is in gesture mode
         if (Get.find<SettingsScreenController>().playerUi.value == 1) {
           gesturePlayerVisibleState.value = 2;
         }
@@ -319,16 +315,12 @@ class PlayerController extends GetxController
     });
   }
 
-  ///pushSongToPlaylist method clear previous song queue, plays the tapped song and push related
-  ///songs into Queue
   Future<void> pushSongToQueue(MediaItem? mediaItem,
       {String? playlistid, bool radio = false}) async {
-    /// update playing from value
     playinfrom.value = PlaylingFrom(
         type: PlaylingFromType.SELECTION,
         name: radio ? "randomRadio".tr : "randomSelection".tr);
 
-    /// set global radio mode flag
     isRadioModeOn = radio;
 
     Future.delayed(
@@ -343,8 +335,6 @@ class PlayerController extends GetxController
           await _audioHandler.customAction("shuffleCmd", {"index": 0});
         }
 
-        // added here to broadcast current mediaitem via Audio Service as list is updated
-        // if radio is started on current playing song
         if (radio && (currentSong.value?.id == mediaItem?.id)) {
           _audioHandler
               .customAction("upadateMediaItemInAudioService", {"index": 0});
@@ -367,12 +357,10 @@ class PlayerController extends GetxController
       return;
     }
 
-    //currentSong.value = mediaItem;
     _playerPanelCheck();
     await _audioHandler
         .customAction("setSourceNPlay", {'mediaItem': mediaItem});
 
-    // disable queue loop mode when radio is started
     if (radio &&
         isQueueLoopModeEnabled.isTrue &&
         isShuffleModeEnabled.isFalse) {
@@ -383,13 +371,10 @@ class PlayerController extends GetxController
   Future<void> playPlayListSong(List<MediaItem> mediaItems, int index,
       {PlaylingFrom? playfrom}) async {
     isRadioModeOn = false;
-    //open player pane,set current song and push first song into playing list,
 
-    /// update playing from value
     playinfrom.value =
         playfrom ?? PlaylingFrom(type: PlaylingFromType.SELECTION);
 
-    //for changing home content based on last interation
     Future.delayed(const Duration(seconds: 3), () {
       if (Hive.box("AppPrefs").get("discoverContentType") == "BOLI") {
         Get.find<HomeScreenController>()
@@ -410,6 +395,30 @@ class PlayerController extends GetxController
     await pushSongToQueue(mediaItem, playlistid: playlistid, radio: true);
   }
 
+  // --- MÉTODO PARA REPRODUCIR RADIOS EN VIVO DE PARAGUAY ---
+  Future<void> playRadio(String streamUrl, String name) async {
+    isRadioModeOn = false;
+    playinfrom.value = PlaylingFrom(
+      type: PlaylingFromType.SELECTION, 
+      name: name
+    );
+
+    final radioItem = MediaItem(
+      id: streamUrl,
+      title: name,
+      artist: 'Radio en Vivo 📻',
+      artUri: Uri.parse('https://cdn-icons-png.flaticon.com/512/2995/2995105.png'),
+      extras: {
+        'url': streamUrl,
+        'streamUrl': streamUrl,
+      },
+    );
+
+    _playerPanelCheck();
+    await _audioHandler.updateQueue([radioItem]);
+    await _audioHandler.customAction("playByIndex", {"index": 0});
+  }
+
   Future<void> _addRadioContinuation(dynamic item) async {
     final isSong = item.runtimeType.toString() == "MediaItem";
     final content = await _musicServices.getWatchPlaylist(
@@ -422,20 +431,16 @@ class PlayerController extends GetxController
     await enqueueSongList(List<MediaItem>.from(content['tracks']));
   }
 
-  ///enqueueSong   append a song to current queue
-  ///if current queue is empty, push the song into Queue and play that song
   Future<void> enqueueSong(MediaItem mediaItem) async {
     if (currentQueue.isEmpty) {
       await playPlayListSong([mediaItem], 0);
       return;
     }
-    //check if song is available in queue and if not add it to queue
     if (!currentQueue.contains(mediaItem)) {
       _audioHandler.addQueueItem(mediaItem);
     }
   }
 
-  ///enqueueSongList method add song List to current queue
   Future<void> enqueueSongList(List<MediaItem> mediaItems) async {
     if (currentQueue.isEmpty) {
       await playPlayListSong(mediaItems, 0);
@@ -492,7 +497,6 @@ class PlayerController extends GetxController
       }
       onReorder(index, currentSongIndex.value + 1);
     } else {
-      //Will add song just below the current song
       (currentIndx == currentQueue.length - 1)
           ? enqueueSong(song)
           : _audioHandler.customAction("addPlayNextItem", {"mediaItem": song});
@@ -539,7 +543,6 @@ class PlayerController extends GetxController
         : _audioHandler.setShuffleMode(AudioServiceShuffleMode.all);
     isShuffleModeEnabled.value = !shuffleModeEnabled;
     await Hive.box("AppPrefs").put("isShuffleModeEnabled", !shuffleModeEnabled);
-    // restrict queue loop mode when shuffle mode is enabled
     if (isShuffleModeEnabled.isTrue && isQueueLoopModeEnabled.isFalse) {
       isQueueLoopModeEnabled.value = true;
     } else if (isShuffleModeEnabled.isFalse) {
@@ -572,7 +575,6 @@ class PlayerController extends GetxController
   void playPause() {
     if (initFlagForPlayer) return;
     _audioHandler.playbackState.value.playing ? pause() : play();
-    // for gesture player
     if (Get.find<SettingsScreenController>().playerUi.value == 1) {
       gesturePlayerVisibleState.value =
           _audioHandler.playbackState.value.playing ? 0 : 1;
@@ -679,8 +681,6 @@ class PlayerController extends GetxController
               action: 'add', index: 0)
           : playlistController.addNRemoveItemsinList(currMediaItem,
               action: 'remove');
-
-      // ignore: empty_catches
     } catch (e) {}
     isCurrentSongFav.value = !isCurrentSongFav.value;
     if (Get.find<SettingsScreenController>()
@@ -691,10 +691,8 @@ class PlayerController extends GetxController
     }
   }
 
-  // ignore: prefer_typing_uninitialized_variables
   var recentItem;
 
-  /// This function is used to add a mediaItem/Song to Recently played playlist
   Future<void> _addToRP(MediaItem mediaItem) async {
     if (recentItem != mediaItem) {
       final box = await Hive.openBox("LIBRP");
@@ -717,14 +715,10 @@ class PlayerController extends GetxController
           playlistController.songList
               .removeWhere((element) => element.id == removedSongId);
         }
-        // removes current duplicate item from list
         playlistController.songList
             .removeWhere((element) => element.id == mediaItem.id);
-        // adds current item to list
         playlistController.addNRemoveItemsinList(mediaItem,
             action: 'add', index: 0);
-
-        // ignore: empty_catches
       } catch (e) {}
     }
     recentItem = mediaItem;
@@ -806,8 +800,6 @@ class PlayerController extends GetxController
     await _audioHandler.customAction("openEqualizer");
   }
 
-  /// Called from audio handler in case audio is not playable
-  /// or returned streamInfo null due to network error
   void notifyPlayError(String message) {
     ScaffoldMessenger.of(Get.context!).showSnackBar(snackbar(
         Get.context!, message == "networkError" ? message.tr : message,
@@ -824,7 +816,6 @@ class PlayerController extends GetxController
     if (GetPlatform.isWindows) {
       Get.delete<WindowsAudioService>();
     }
-    // ensure wakelock disabled when player controller disposed
     try {
       _setWakelock(false);
     } catch (e) {
